abe045999f9da4c14ba9378a069731509ff4724e
prateekparallel <prateekparallel@gmail.com>
2018-09-01 12:03:32 +0100

    added StudentRegistrationReply class

---
+++ src/main/java/com/bhaiti/kela/beans/StudentRegistrationReply.java
@@ -0,0 +1,35 @@
+package com.bhaiti.kela.beans;
+
+public class StudentRegistrationReply {
+	
+	String name;
+	int age;
+	String registrationNumber;
+	String registrationStatus;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getRegistrationNumber() {
+		return registrationNumber;
+	}
+	public void setRegistrationNumber(String registrationNumber) {
+		this.registrationNumber = registrationNumber;
+	}
+	public String getRegistrationStatus() {
+		return registrationStatus;
+	}
+	public void setRegistrationStatus(String registrationStatus) {
+		this.registrationStatus = registrationStatus;
+	}	
+
+}
