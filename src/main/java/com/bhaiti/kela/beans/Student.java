c50b26208456f98f228a9613cacb8506d74cca22
prateekparallel <prateekparallel@gmail.com>
2018-09-01 11:58:48 +0100

    added Student class

---
+++ src/main/java/com/bhaiti/kela/beans/Student.java
@@ -0,0 +1,27 @@
+package com.bhaiti.kela.beans;
+
+public class Student {
+	
+	String name;
+	int age;
+	String registrationNumber;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getRegistrationNumber() {
+		return registrationNumber;
+	}
+	public void setRegistrationNumber(String registrationNumber) {
+		this.registrationNumber = registrationNumber;
+	}
+}
