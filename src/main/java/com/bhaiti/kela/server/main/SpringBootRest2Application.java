cb335a2f8981d9a866a080c0a2a195a27896a828
prateekparallel <prateekparallel@gmail.com>
2018-09-01 11:45:39 +0100

    added base package path

--- src/main/java/com/bhaiti/kela/server/main/SpringBootRest2Application.java
+++ src/main/java/com/bhaiti/kela/server/main/SpringBootRest2Application.java
@@ -3,7 +3,7 @@ package com.bhaiti.kela.server.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.bhaiti"})
 public class SpringBootRest2Application {
 
 	public static void main(String[] args) {
