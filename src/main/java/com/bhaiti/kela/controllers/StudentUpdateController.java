f02e3534873eb8d1085bba5b4a4aa62343a05092
prateekparallel <prateekparallel@gmail.com>
2018-09-01 12:09:25 +0100

    added StudentUpdateController class

---
+++ src/main/java/com/bhaiti/kela/controllers/StudentUpdateController.java
@@ -0,0 +1,26 @@
+package com.bhaiti.kela.controllers;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bhaiti.kela.beans.Student;
+import com.bhaiti.kela.beans.StudentRegistration;
+
+@Controller
+public class StudentUpdateController {
+	
+@RequestMapping(method = RequestMethod.POST, value="/update/student")
+
+	
+	@ResponseBody
+	public String updateStudentRecord(@RequestBody Student stdn) {		
+		System.out.println("In updateStudentRecord");	   
+	    return StudentRegistration.getInstance().upDateStudent(stdn);		
+	}	
+
+}
